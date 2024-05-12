e0f583572ab9fcb22bce8426578dfb8289d0d92f
Developer-Jihad <jihad.developer.bd@gmail.com>
2024-05-12 15:09:41 +0600

    env file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -9,13 +10,13 @@ const PORT = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 
-// Jihad
-// mvMf958ghvfTTCWO
+//
+//
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const uri =
-  "mongodb+srv://Jihad:mvMf958ghvfTTCWO@cluster0.szoaovn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szoaovn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
